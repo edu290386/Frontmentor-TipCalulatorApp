d72b12ac49c74c96f25ac143307611cfdd4c7761
edu290386 <eduardo.torres@outlook.com>
2023-07-22 02:10:25 -0500

    mobile first

--- src/components/InputCard.jsx
+++ src/components/InputCard.jsx
@@ -2,20 +2,20 @@
 
 const InputCard = () => {
   return (
-    <div className='bg-white h-[350px] rounded-t-[40px] p-6'>
+    <div className='bg-white h-[320px] rounded-t-2xl p-6'>
       <h2>Bill</h2>
       <input type="tel" placeholder="$"/>
       <h2>Select Tip %</h2>
-      <div className="grid grid-cols-2 gap-4 text-white text-center text-2xl font-bold  ">
-        <div className="bg-[#0c494c] py-2 rounded-lg hover:bg-[#30c1ad] hover:text-[#0c494c] hover:font-bold">5%</div>
-        <div className="bg-[#0c494c] py-2 rounded-lg hover:bg-[#30c1ad] hover:text-[#0c494c] hover:font-bold">10%</div>
-        <div className="bg-[#0c494c] py-2 rounded-lg hover:bg-[#30c1ad] hover:text-[#0c494c] hover:font-bold">15%</div>
-        <div className="bg-[#0c494c] py-2 rounded-lg hover:bg-[#30c1ad] hover:text-[#0c494c] hover:font-bold">25%</div>
-        <div className="bg-[#0c494c] py-2 rounded-lg hover:bg-[#30c1ad] hover:text-[#0c494c] hover:font-bold"> 50%</div>
-        <input type="tel" className="text-2xl rounded-lg text-[#0c494c] bg-[#f2f8fb] indent-12" placeholder="Custom"/>
+      <div className="grid grid-cols-2 gap-2 text-white text-center text-2xl font-bold  ">
+        <div className="bg-[#0c494c] py-1 rounded-lg hover:bg-[#30c1ad] hover:text-[#0c494c] hover:font-bold">5%</div>
+        <div className="bg-[#0c494c] py-1 rounded-lg hover:bg-[#30c1ad] hover:text-[#0c494c] hover:font-bold">10%</div>
+        <div className="bg-[#0c494c] py-1 rounded-lg hover:bg-[#30c1ad] hover:text-[#0c494c] hover:font-bold">15%</div>
+        <div className="bg-[#0c494c] py-1 rounded-lg hover:bg-[#30c1ad] hover:text-[#0c494c] hover:font-bold">25%</div>
+        <div className="bg-[#0c494c] py-1 rounded-lg hover:bg-[#30c1ad] hover:text-[#0c494c] hover:font-bold"> 50%</div>
+        <input type="tel" className="text-xl rounded-lg text-[#0c494c] bg-[#f2f8fb] indent-16" placeholder="Custom"/>
       </div>
-      <h2 className="pt-4 pb-2">Number of People</h2>
-      <input type="tel" className="w-full py-2 text-2xl rounded-lg text-[#0c494c] bg-[#f2f8fb] indent-48" placeholder=""/>
+      <h2 className="pt-2 pb-2">Number of People</h2>
+      <input type="tel" className="w-full py-1 text-xl rounded-lg text-[#0c494c] bg-[#f2f8fb] indent-48" placeholder=""/>
     </div>
   )
 }
--- src/components/LabelCard.jsx
+++ src/components/LabelCard.jsx
@@ -3,13 +3,15 @@ import InformationCard from './InformationCard'
 
 const LabelCard = () => {
   return (
-    <div className='w-[375px] font-space-mono bg-white   tracking-widest '>
-        <div className='text-3xl text-[#0c494c] text-center h-[60px] bg-[#c4e3e7]'>
-            <div>SPLI</div>
-            <div>TTER</div>
-        </div>
-        <div className=''>
+    <div className='w-[375px] font-space-mono   '>
+        <div className=' text-[#0c494c]   bg-[#c4e3e7]'>
+            <div className='text-center tracking-widest text-3xl'>
+              <div>SPLI</div>
+              <div>TTER</div>
+            </div>
             <InputCard />
+        </div>
+        <div className='bg-white'>
             <InformationCard />
         </div>
         
