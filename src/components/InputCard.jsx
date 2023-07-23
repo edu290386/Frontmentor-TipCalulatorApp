e46a7d587467bd24a3cc698c46e142b5e1805597
edu290386 <eduardo.torres@outlook.com>
2023-07-23 18:12:28 -0500

    improve inputs

--- src/components/InputCard.jsx
+++ src/components/InputCard.jsx
@@ -1,8 +1,28 @@
 import dollar from '../assets/icon-dollar.svg'
 import person from '../assets/icon-person.svg'
 import InformationCard from './InformationCard'
+import { useState } from 'react'
 
 const InputCard = () => {
+  const percentages = [5,10,15,25,50]
+  const [values, setValues] = useState({}) 
+
+  const handleChange = (e) => {
+    let {name, value} = e.target
+    if(!isNaN(value)){
+      setValues({...values, [name]:value})
+    }
+  
+
+    
+  }
+
+  const selectTip = (value) => {
+    setValues({...values, ["percentage"]:value})
+  }
+
+  console.log(values)
+
   return (
     <div className='flex flex-col md:flex-row'>
       <div className="bg-[#c4e3e7]">
@@ -10,21 +30,21 @@ const InputCard = () => {
           <h2 className="my-2 ">Bill</h2>
           <div className='relative'>
             <img src={dollar} className='absolute top-4 left-4' alt="" />
-            <input type="tel" className="w-full py-2 text-2xl text-right font-bold rounded-lg text-[#0c494c] bg-[#f3f8fb] pr-4 focus:outline-[#30c1ad]" placeholder="0"/>
+            <input type="tel" onChange={handleChange} name="amount" value={values.amount} className="w-full py-2 text-2xl text-right font-bold rounded-lg text-[#0c494c] bg-[#f3f8fb] pr-4 focus:outline-[#30c1ad]" placeholder="0"/>
           </div>
           <h2 className="my-5 pt-3 md:my-2">Select Tip %</h2>
           <div className="grid grid-cols-2 gap-4 text-white text-center text-2xl font-semibold md:grid-cols-3 ">
-            <div className="bg-[#0c494c] py-2 rounded-lg hover:bg-[#30c1ad] hover:text-[#0c494c] hover:font-semibold hover:cursor-pointer ">5%</div>
-            <div className="bg-[#0c494c] py-2 rounded-lg hover:bg-[#30c1ad] hover:text-[#0c494c] hover:font-semibold hover:cursor-pointer ">10%</div>
-            <div className="bg-[#0c494c] py-2 rounded-lg hover:bg-[#30c1ad] hover:text-[#0c494c] hover:font-semibold hover:cursor-pointer ">15%</div>
-            <div className="bg-[#0c494c] py-2 rounded-lg hover:bg-[#30c1ad] hover:text-[#0c494c] hover:font-semibold hover:cursor-pointer ">25%</div>
-            <div className="bg-[#0c494c] py-2 rounded-lg hover:bg-[#30c1ad] hover:text-[#0c494c] hover:font-semibold hover:cursor-pointer "> 50%</div>
-            <input type="tel" className="text-xl rounded-lg text-[#0c494c] bg-[#f2f8fb] focus:outline-[#30c1ad] text-center hover:cursor-pointer " placeholder="Custom"/>
+            {
+              percentages.map((percentage, index) => (
+                <button onClick={() => selectTip(percentage)} key={index} name="percentage" className={values.percentage == percentage ? "bg-[#30c1ad] text-[#0c494c] font-semibold py-2 rounded-lg" : "bg-[#0c494c] py-2 rounded-lg"} >{percentage}%</button>
+              ))
+            }
+            <input type="tel" onChange={handleChange} name="custom" value={values.custom || ''} className="text-xl rounded-lg text-[#0c494c] bg-[#f2f8fb] focus:outline-[#30c1ad] text-center hover:cursor-pointer " placeholder="Custom"/>
           </div>
           <h2 className="my-2 pt-4 ">Number of People</h2>
           <div className='relative'>
             <img src={person} className='absolute top-4 left-4' alt="" />
-            <input type="tel" className="w-full py-2 text-2xl text-right font-bold rounded-lg text-[#0c494c] bg-[#f3f8fb] pr-4 focus:outline-[#30c1ad]" placeholder="0"/>
+            <input type="tel" onChange={handleChange} name="people" value={values.people || ''} className="w-full py-2 text-2xl text-right font-bold rounded-lg text-[#0c494c] bg-[#f3f8fb] pr-4 focus:outline-[#30c1ad]" placeholder="0"/>
           </div>
         </div>
       </div>
